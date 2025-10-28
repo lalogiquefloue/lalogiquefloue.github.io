55f6a3dcb11eb77f4e060221b83d8f10a1ad1b46
Carl Thibault <lalogiquefloue@gmail.com>
2025-10-28 09:23:41 -0400

    add empty navbar rendering to avoid flicker, fix missing cache key

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,5 +1,6 @@
 export async function loadNavbar() {
-    let navbarHTML = localStorage.getItem("NAVBAR_HTML");
+    const cacheKey = "NAVBAR_HTML";
+    let navbarHTML = localStorage.getItem(cacheKey);
 
     // cache navbar HTML if not already in local storage
     if (!navbarHTML) {
@@ -12,29 +13,27 @@ export async function loadNavbar() {
     const navBar = document.getElementById("navbar-placeholder");
     navBar.innerHTML = navbarHTML;
 
-    requestAnimationFrame(() => {
-        // Menu logic
-        const menuBtn = document.getElementById("menu-btn");
-        const mobileMenu = document.getElementById("mobile-menu");
-        const iconOpen = document.getElementById("icon-open");
-        const iconClose = document.getElementById("icon-close");
+    // Menu logic
+    const menuBtn = document.getElementById("menu-btn");
+    const mobileMenu = document.getElementById("mobile-menu");
+    const iconOpen = document.getElementById("icon-open");
+    const iconClose = document.getElementById("icon-close");
 
-        if (menuBtn) {
-            menuBtn.addEventListener("click", () => {
-                mobileMenu.classList.toggle("hidden");
-                iconOpen.classList.toggle("hidden");
-                iconClose.classList.toggle("hidden");
-            });
-        }
-
-        // Highlight current page
-        const navLinks = navBar.querySelectorAll("nav a");
-        const currentPath = window.location.pathname.split("/").pop() || "index.html";
-
-        navLinks.forEach(link => {
-            if (link.getAttribute("href") === currentPath) {
-                link.classList.add("border-black", "font-bold");
-            }
+    if (menuBtn) {
+        menuBtn.addEventListener("click", () => {
+            mobileMenu.classList.toggle("hidden");
+            iconOpen.classList.toggle("hidden");
+            iconClose.classList.toggle("hidden");
         });
+    }
+
+    // Highlight current page
+    const navLinks = navBar.querySelectorAll("nav a");
+    const currentPath = window.location.pathname.split("/").pop() || "index.html";
+
+    navLinks.forEach(link => {
+        if (link.getAttribute("href") === currentPath) {
+            link.classList.add("border-black", "font-bold");
+        }
     });
 }
